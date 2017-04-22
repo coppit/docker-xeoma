51fe72872a537f4af223a90a5e80122aa297fc91
David Coppit <david@coppit.org>
2017-04-22 11:47:45 -0400

    Improved build file

--- build.sh
+++ build.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
 
-docker build --rm=true -t coppit/xeoma . 
+# Example: build.sh
+# Example: build.sh v1
+
+echo "Building locally..."
+docker build -t coppit/xeoma .
+docker tag coppit/xeoma coppit/xeoma:dev
+
+if [[ "$#" == 1 ]]; then
+    RELEASE=$1
+
+    VERSION_URL=$(curl -s http://felenasoft.com/xeoma/en/download/ | grep 'images/theme/download/version.js' | sed 's|[^"]*"\([^"]*\)".*|http://felenasoft.com\1|')
+    STABLE_VERSION=$(curl -s $VERSION_URL | grep 'var version ' | sed 's/.*"\(.*\)".*/\1/')
+    BETA_VERSION=$(curl -s $VERSION_URL | grep 'var version_beta ' | sed 's/.*"\(.*\)".*/\1/')
+
+    TAG="stable${STABLE_VERSION}_beta${BETA_VERSION}_$RELEASE"
+
+    echo "Tagging and pushing..."
+    docker tag coppit/xeoma:latest coppit/xeoma:$TAG
+    docker push coppit/xeoma:latest
+    docker push coppit/xeoma:$TAG
+fi
