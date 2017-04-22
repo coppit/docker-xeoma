04b99ff6389b10c08a227a7a1df63982cc859e57
David Coppit <david@coppit.org>
2017-04-22 16:27:32 -0400

    Fix failure to connect problem.

--- xeoma.sh
+++ xeoma.sh
@@ -23,5 +23,8 @@ echo "$(ts) Using the $VERSION version of Xeoma"
 echo "$(ts) Setting the password"
 /files/$VERSION/xeoma.app -setpassword "$PASSWORD"
 
+# Not sure why this is necessary, but without it, I can't connect to the server
+/files/$VERSION/xeoma.app -showpassword > /dev/null 2>&1
+
 echo "$(ts) Starting the server in 5 seconds..."
 /files/$VERSION/xeoma.app -core -service -log -startdelay 5
