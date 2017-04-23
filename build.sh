87c717657a54ada33c5be954fed9aee49cd41d1f
David Coppit <david@coppit.org>
2017-04-23 17:24:29 -0400

    Show versions when building locally

--- build.sh
+++ build.sh
@@ -7,12 +7,14 @@ echo "Building locally..."
 docker build -t coppit/xeoma .
 docker tag coppit/xeoma coppit/xeoma:dev
 
+VERSION_URL=$(curl -s http://felenasoft.com/xeoma/en/download/ | grep 'images/theme/download/version.js' | sed 's|[^"]*"\([^"]*\)".*|http://felenasoft.com\1|')
+STABLE_VERSION=$(curl -s $VERSION_URL | grep 'var version ' | sed 's/.*"\(.*\)".*/\1/')
+BETA_VERSION=$(curl -s $VERSION_URL | grep 'var version_beta ' | sed 's/.*"\(.*\)".*/\1/')
+
+echo "Local version built, tagged as coppit/xeoma:dev. Stable version is $STABLE_VERSION. Beta version is $BETA_VERSION"
+
 if [[ "$#" == 1 ]]; then
     RELEASE=$1
-
-    VERSION_URL=$(curl -s http://felenasoft.com/xeoma/en/download/ | grep 'images/theme/download/version.js' | sed 's|[^"]*"\([^"]*\)".*|http://felenasoft.com\1|')
-    STABLE_VERSION=$(curl -s $VERSION_URL | grep 'var version ' | sed 's/.*"\(.*\)".*/\1/')
-    BETA_VERSION=$(curl -s $VERSION_URL | grep 'var version_beta ' | sed 's/.*"\(.*\)".*/\1/')
 
     TAG="stable${STABLE_VERSION}_beta${BETA_VERSION}_$RELEASE"
 
