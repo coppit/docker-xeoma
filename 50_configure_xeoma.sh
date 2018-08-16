11254f0f17315b963a952e0f75bb8fb90d3982ac
David Coppit <david@coppit.org>
2018-08-15 21:06:45 -0400

    Move to Alpine linux
    Based on https://github.com/sanjeevan/baseimage
    
    Image size reduced from 229MB to 64MB
    
    Also:
    - Move config file handling to a run-once script
    - Better support for using env vars to set values instead of config file

--- 50_configure_xeoma.sh
+++ 50_configure_xeoma.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-set -e
-
-FIXED_CONFIG_FILE=/tmp/xeoma.conf
 MAC_FILE=/config/macs.txt
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+. /etc/envvars.merged
+
+set -e
 
 #-----------------------------------------------------------------------------------------------------------------------
 
 function ts {
   echo [`date '+%b %d %X'`]
 }
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-source "$FIXED_CONFIG_FILE"
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -26,27 +25,27 @@ echo "$(ts) $iface $mac_address" >> "$MAC_FILE"
 
 #-----------------------------------------------------------------------------------------------------------------------
 
-# If we were to install Xeoma, it would run in /usr/local/Xeoma. But we're not, so it runs in /.config
-mkdir -p /.config
-
 # Delete before creating the symlinks, for two reasons: (1) the symlink might be left-over from a previous container (and
 # therefore invalid in this container), and (2) if the container is restarted, there will already be a symlink, causing
 # a new symlink like /usr/local/Xeoma/config/config
 
+# Clean up any mess from before
+rm -f /config/config
+
 # NOTE: Around version 18.7.10 /.config is no longer used. Instead /usr/local/Xeoma is used whether or not the software
 # is installed.
 
-# Old code below (for backwards compatibility)
+# == Old code below (for backwards compatibility)
+# If we were to install Xeoma, it would run in /usr/local/Xeoma. But we're not, so it runs in /.config
+mkdir -p /.config
+
 rm -f /.config/Xeoma
 ln -s /config /.config/Xeoma
 
 rm -f /config/XeomaArchive
 ln -s /archive /config/XeomaArchive
 
-# Clean up any mess from before
-rm -f /config/config
-
-# New code below.
+# == New code below.
 rm -f /usr/local/Xeoma
 ln -s /config /usr/local/Xeoma
 
---
+++ parse_config_file.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+# Container-specific stuff first
+
+REQUIRED_SETTINGS="PASSWORD"
+DEFAULT_SETTINGS="VERSION=latest"
+
+TEMPLATE_CONFIG_FILE=/files/xeoma.conf.default
+CONFIG_FILE=/config/xeoma.conf
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+validate_values() {
+  if [ $(all_required_settings_exist) != true ]
+  then
+    echo "Missing required settings, which must be provided in the config file or by environment variables:"
+    echo "$REQUIRED_SETTINGS"
+    exit 0
+  fi
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+print_config() {
+  echo "Configuration:"
+  echo "  PASSWORD=<hidden>"
+  echo "  VERSION=$VERSION"
+}
+
+########################################################################################################################
+
+ENV_VARS=/etc/envvars
+MERGED_ENV_VARS=/etc/envvars.merged
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+all_required_settings_exist() {
+  ALL_REQUIRED_SETTINGS_EXIST=true
+  for required_setting in $REQUIRED_SETTINGS
+  do
+    if [ -z "$(eval "echo \$$required_setting")" ]
+    then
+      ALL_REQUIRED_SETTINGS_EXIST=false
+      break
+    fi
+  done
+
+  echo $ALL_REQUIRED_SETTINGS_EXIST
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+create_and_validate_config_file() {
+  # Search for config file. If it doesn't exist, copy the default one
+  if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Creating config file. Please do not forget to edit it to specify your settings!"
+    cp "$TEMPLATE_CONFIG_FILE" "$CONFIG_FILE"
+    chmod a+w "$CONFIG_FILE"
+    exit 1
+  fi
+
+  # Check to see if they didn't edit the config file
+  if diff "$TEMPLATE_CONFIG_FILE" "$CONFIG_FILE" >/dev/null
+  then
+    echo "Please edit the config file to specify your settings"
+    exit 3
+  fi
+
+  # Translate line endings, since they may have edited the file in Windows
+  PROCESSED_CONFIG_FILE=$(mktemp)
+
+  tr -d '\r' < "$CONFIG_FILE" > "$PROCESSED_CONFIG_FILE"
+
+  echo "$PROCESSED_CONFIG_FILE"
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+merge_config_vars_and_env_vars() {
+  SAFE_CONFIG_FILE=$1
+
+  . "$SAFE_CONFIG_FILE"
+  export $(grep = "$SAFE_CONFIG_FILE" | grep -v '^ *#' | cut -d= -f1)
+
+  # Env vars take precedence
+  . "$ENV_VARS"
+  export $(cut -d= -f1 "$ENV_VARS")
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+set_default_values() {
+  # Handle defaults now
+  for KEY_VALUE in $DEFAULT_SETTINGS
+  do
+    KEY=$(echo "$KEY_VALUE" | cut -d= -f1)
+    VALUE=$(echo "$KEY_VALUE" | cut -d= -f2)
+
+    eval "export $KEY=\${$KEY:=$VALUE}"
+  done
+}
+
+########################################################################################################################
+
+. "$ENV_VARS"
+
+if [ $(all_required_settings_exist) = true ]
+then
+  echo "All required settings passed as environment variables. Skipping config file creation."
+  exit 0
+fi
+
+SAFE_CONFIG_FILE=$(create_and_validate_config_file)
+
+merge_config_vars_and_env_vars $SAFE_CONFIG_FILE
+
+validate_values
+
+set_default_values
+
+print_config
+
+# Now dump the envvars, in the style that boot.sh does. exec to avoid SHLVL=2
+exec sh -c "export > \"$MERGED_ENV_VARS\""
